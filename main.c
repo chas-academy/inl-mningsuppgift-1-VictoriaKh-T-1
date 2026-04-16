74f9eb3ec0d7589e7bd98368a391351f25061d14
Victoriakh_T <kaplabetty@gmail.com>
2026-04-16 14:44:58 +0200

    solution

--- main.c
+++ main.c
@@ -3,11 +3,60 @@
 
 int main()
 {
-    // The Line below must *NOT* be changed or test will fail!
-    srand(1);
+  // The Line below must *NOT* be changed or test will fail!
+  srand(1);
 
-   
-    // Your code goes here
+  int numbers[100];
+  int counts[6] = {0, 0, 0, 0, 0, 0};
+  int sum = 0;
 
-    return 0;
+  for (int i = 0; i < 100; i++)
+  {
+    numbers[i] = (rand() % 6) + 1;
+    sum += numbers[i];
+    /*  if (numbers[i] == 1)
+        {
+          counts[0]++;
+        }else if (numbers[i] == 2) {
+          counts[1]++;
+        }else if (numbers[i] == 3) {
+          counts[2]++;
+        }else if (numbers[i] == 4) {
+          counts[3]++;
+        }else if (numbers[i] == 5) {
+          counts[4]++;
+        }else {
+          counts[5]++;
+        } */
+    switch (numbers[i])
+    {
+    case 1:
+      counts[0]++;
+      break;
+    case 2:
+      counts[1]++;
+      break;
+    case 3:
+      counts[2]++;
+      break;
+    case 4:
+      counts[3]++;
+      break;
+    case 5:
+      counts[4]++;
+      break;
+    case 6:
+      counts[5]++;
+      break;
+    }
+  }
+
+  for (int i = 0; i < 6; i++)
+  {
+    printf("%d\n", counts[i]);
+  }
+
+  printf("Total: %d\n", sum);
+  printf("%.1f", sum / 100.0);
+  return 0;
 }
